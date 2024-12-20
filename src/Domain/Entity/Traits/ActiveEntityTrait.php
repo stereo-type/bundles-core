98bd1110f8d70cfce5f4385585f62d6b2c78008f
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2024-12-20 13:36:01 +0300

    ARH-436 active trait

---
+++ src/Domain/Entity/Traits/ActiveEntityTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CoreBundle\Domain\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * "Стандартное поле" delete для Entity
+ */
+trait ActiveEntityTrait
+{
+    #[ORM\Column(options: ['default' => true])]
+    private bool $active = true;
+
+    public function isActive(): bool
+    {
+        return $this->active;
+    }
+
+    public function setActive(bool $active): static
+    {
+        $this->active = $active;
+
+        return $this;
+    }
+}
