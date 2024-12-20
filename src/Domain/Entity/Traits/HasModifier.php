1867109dedd3bc6fbaa4033a9ec010c391ab7897
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2024-12-20 15:26:29 +0300

    ARH-436 security install

---
+++ src/Domain/Entity/Traits/HasModifier.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CoreBundle\Domain\Entity\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\PrePersist;
+use Doctrine\ORM\Mapping\PreUpdate;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**Трейт для подключения к сущности и создания полей
+ * $user_created и $user_modified
+ * отдельно ничего не надо вызывать и устанавливать, работает по событиям доктрины
+ * */
+trait HasModifier
+{
+
+    private ?UserInterface $user = null;
+
+    public function __construct(?UserInterface $user)
+    {
+        $this->user = $user;
+    }
+
+    #[ORM\ManyToOne]
+    private ?UserInterface $user_created = null;
+
+    #[ORM\ManyToOne]
+    private ?UserInterface $user_modified = null;
+
+    #[PrePersist, PreUpdate]
+    public function updateModifier(): void
+    {
+        if ($this->user) {
+            if (!isset($this->user_created)) {
+                $this->user_created = $this->user;
+            }
+
+            $this->user_modified = $this->user;
+        }
+    }
+
+    public function getUserCreated(): ?UserInterface
+    {
+        return $this->user_created;
+    }
+
+    public function setUserCreated(?UserInterface $user_created): self
+    {
+        $this->user = $user_created;
+        $this->user_created = $user_created;
+
+        return $this;
+    }
+
+    public function getUserModified(): ?UserInterface
+    {
+        return $this->user_modified;
+    }
+
+    public function setUserModified(?UserInterface $user_modified): self
+    {
+        $this->user = $user_modified;
+        $this->user_modified = $user_modified;
+
+        return $this;
+    }
+
+
+}
