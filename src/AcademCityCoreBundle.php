9fcb682fa36e35847728910c3e9dc9d9e734b1c3
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2024-12-24 10:30:04 +0300

    ARH-449 recipe4

--- src/AcademCityCoreBundle.php
+++ src/AcademCityCoreBundle.php
@@ -12,10 +12,38 @@ use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class AcademCityCoreBundle extends AbstractBundle
 {
+    private const PERMISSIONS_MASK = 0755;
+
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $filesystem = new Filesystem();
+        $container = $this->container;
+
+        $projectRoot = $container->getParameter('kernel.project_dir');
+        $subDir = "/config/packages";
+        $filename = "academcity_core.yaml";
+        $projectConfigDir = $projectRoot . $subDir;
+        $targetConfigFile = $projectConfigDir . "/$filename";
+        $bundleConfigFile = __DIR__ . "/../Resources/config/packages/$filename";
+
+        if (!$filesystem->exists($projectConfigDir)) {
+            $filesystem->mkdir($projectConfigDir, self::PERMISSIONS_MASK);
+        }
+
+        if (!$filesystem->exists($targetConfigFile)) {
+            $filesystem->copy($bundleConfigFile, $targetConfigFile);
+        }
+    }
+
     public function configure(DefinitionConfigurator $definition): void
     {
         /** @var ArrayNodeDefinition $root*/
