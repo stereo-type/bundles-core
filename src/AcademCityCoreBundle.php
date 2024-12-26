9e6db69ac62a8b41d35e1722be63c95106c01efb
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2024-12-26 17:39:25 +0300

    ARH-476 1.3.7 еще попытка

--- src/AcademCityCoreBundle.php
+++ src/AcademCityCoreBundle.php
@@ -9,37 +9,15 @@ declare(strict_types=1);
 namespace AcademCity\CoreBundle;
 
 use AcademCity\CoreBundle\DependencyInjection\AcademCityCoreExtension;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class AcademCityCoreBundle extends AbstractBundle
 {
-    private const PERMISSIONS_MASK = 0755;
-
     public function getContainerExtension(): ?ExtensionInterface
     {
         return new AcademCityCoreExtension();
     }
 
-    public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $filesystem = new Filesystem();
-        $projectRoot = $builder->getParameter('kernel.project_dir');
-        $subDir = DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'packages';
-        $filename = 'academ_city_core.yaml';
-        $projectConfigDir = $projectRoot . $subDir;
-        $targetConfigFile = $projectConfigDir . DIRECTORY_SEPARATOR . $filename;
-        $bundleConfigFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources' . $subDir . DIRECTORY_SEPARATOR . $filename;
-        if (!$filesystem->exists($projectConfigDir)) {
-            $filesystem->mkdir($projectConfigDir, self::PERMISSIONS_MASK);
-        }
-
-        if (!$filesystem->exists($targetConfigFile)) {
-            $filesystem->copy($bundleConfigFile, $targetConfigFile);
-        }
-    }
 
 }
--- src/DependencyInjection/AcademCityCoreExtension.php
+++ src/DependencyInjection/AcademCityCoreExtension.php
@@ -13,9 +13,12 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Filesystem\Filesystem;
 
 class AcademCityCoreExtension extends Extension
 {
+    private const PERMISSIONS_MASK = 0755;
+
     public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
@@ -24,6 +27,21 @@ class AcademCityCoreExtension extends Extension
             throw new InvalidArgumentException(
                 'The "user_class" must be configured'
             );
+        }
+
+        $filesystem = new Filesystem();
+        $projectRoot = $container->getParameter('kernel.project_dir');
+        $subDir = DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'packages';
+        $filename = 'academ_city_core.yaml';
+        $projectConfigDir = $projectRoot . $subDir;
+        $targetConfigFile = $projectConfigDir . DIRECTORY_SEPARATOR . $filename;
+        $bundleConfigFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources' . $subDir . DIRECTORY_SEPARATOR . $filename;
+        if (!$filesystem->exists($projectConfigDir)) {
+            $filesystem->mkdir($projectConfigDir, self::PERMISSIONS_MASK);
+        }
+
+        if (!$filesystem->exists($targetConfigFile)) {
+            $filesystem->copy($bundleConfigFile, $targetConfigFile);
         }
 
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'config'));
