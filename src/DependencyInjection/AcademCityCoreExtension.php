471e7c8c490052862871245ca74d770d702f184f
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2024-12-26 17:20:48 +0300

    ARH-449 1.3.5 попытка решить проблему при установке

--- src/DependencyInjection/AcademCityCoreExtension.php
+++ src/DependencyInjection/AcademCityCoreExtension.php
@@ -12,35 +12,15 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\Filesystem\Filesystem;
 
 class AcademCityCoreExtension extends Extension
 {
-    private const PERMISSIONS_MASK = 0755;
-
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $filesystem = new Filesystem();
-        $projectRoot = $container->getParameter('kernel.project_dir');
-        $subDir = DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'packages';
-        $filename = 'academ_city_core.yaml';
-        $projectConfigDir = $projectRoot . $subDir;
-        $targetConfigFile = $projectConfigDir . DIRECTORY_SEPARATOR . $filename;
-        $bundleConfigFile = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'Resources' . $subDir . DIRECTORY_SEPARATOR . $filename;
-        if (!$filesystem->exists($projectConfigDir)) {
-            $filesystem->mkdir($projectConfigDir, self::PERMISSIONS_MASK);
-        }
-
-        if (!$filesystem->exists($targetConfigFile)) {
-            $filesystem->copy($bundleConfigFile, $targetConfigFile);
-        }
-
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader($container, new FileLocator(dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . 'config'));
         $loader->load('services.yaml');
-
         $container->setParameter('academ_city_core.user_class', $config['user_class']);
     }
 }
