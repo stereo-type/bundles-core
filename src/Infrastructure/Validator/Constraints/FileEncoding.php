1e9f85826b0021c8d133b7f232845c05752cf9d2
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2025-05-15 14:57:16 +0300

    ARH-1 1.3.14 encoding utf-8

---
+++ src/Infrastructure/Validator/Constraints/FileEncoding.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package    FileEncoding.php
+ * @copyright  2025 Zhalayletdinov Vyacheslav evil_tut@mail.ru
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace AcademCity\CoreBundle\Infrastructure\Validator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+
+#[\Attribute(\Attribute::TARGET_PROPERTY | \Attribute::TARGET_METHOD | \Attribute::IS_REPEATABLE)]
+class FileEncoding extends Constraint
+{
+    public $message = 'Файл должен быть в кодировке {{ encoding }}.';
+    public $encoding = 'UTF-8';
+}
---
+++ src/Infrastructure/Validator/Constraints/FileEncodingValidator.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package    FileEncodingValidator.php
+ * @copyright  2025 Zhalayletdinov Vyacheslav evil_tut@mail.ru
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace AcademCity\CoreBundle\Infrastructure\Validator\Constraints;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class FileEncodingValidator extends ConstraintValidator
+{
+    /**
+     * Checks if the passed value is valid.
+     *
+     * @return void
+     */
+    public function validate($value, Constraint $constraint)
+    {
+        if (!$value instanceof UploadedFile) {
+            return;
+        }
+
+        $filePath = $value->getRealPath();
+
+        // Определяем кодировку
+        $fileHandle = fopen($filePath, 'r');
+        $sampleData = fread($fileHandle, 1024);
+        fclose($fileHandle);
+
+        $encoding = mb_detect_encoding($sampleData, ['UTF-8', 'Windows-1251', 'ISO-8859-1'], true);
+
+        if ($encoding !== $constraint->encoding) {
+            $this->context->buildViolation($constraint->message)
+                          ->setParameter('{{ encoding }}', $constraint->encoding)
+                          ->addViolation();
+        }
+    }
+}
