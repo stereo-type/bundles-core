1997d3f31554a428162f0804b2a4d2b7d0e11403
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2025-05-15 15:06:23 +0300

    ARH-1 1.3.15 encoding utf-8 2

--- src/Infrastructure/Validator/Constraints/FileEncoding.php
+++ src/Infrastructure/Validator/Constraints/FileEncoding.php
@@ -16,4 +16,13 @@ class FileEncoding extends Constraint
 {
     public $message = 'Файл должен быть в кодировке {{ encoding }}.';
     public $encoding = 'UTF-8';
+
+    public function __construct(string $encoding = 'UTF-8', ?string $message = null, mixed $options = null, ?array $groups = null, mixed $payload = null)
+    {
+        if ($message) {
+            $this->message = $message;
+        }
+        $this->encoding = $encoding;
+        parent::__construct(['encoding' => $encoding, 'message' => $this->message] + ($options ?? []), $groups, $payload);
+    }
 }
